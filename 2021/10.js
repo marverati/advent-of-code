f1daea6a78dfb4b2d5b49aae252508fee021bb1e
marverati <marverati@gmail.com>
2021-12-09 07:29:44 +0100

    Prepare day 10

---
+++ 2021/10.js
@@ -0,0 +1,21 @@
+
+const data0 = ``;
+
+const data1 = require('./10data');
+
+function prepareData(data) {
+  return data.split("\n");
+}
+
+function part1(data) {
+
+}
+
+function part2(data) {
+
+}
+
+
+const data = prepareData(data0);
+console.log(part1(data));
+console.log(part2(data));
