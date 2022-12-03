1dce67ce751a748c92946d4b014e56395ab16d20
marverati <marverati@gmail.com>
2022-12-03 06:14:04 +0100

    Add day 3 2022

---
+++ 2022/03.js
@@ -0,0 +1,61 @@
+require('./_helpers.js');
+const { data1 } = require('./03data');
+
+const data0 = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+function prepareData(data) {
+    const lines = data
+        .split("\n") // turn into array of lines
+        .map(line => line.trim()) // trim white space
+        .filter(line => line !== "") // remove empty lines
+    return lines;
+}
+
+function part1(data) {
+    let result = 0;
+    for (const line of data) {
+        const mid = line.length / 2;
+        const p1 = line.substring(0, mid);
+        const p2 = line.substring(mid);
+        // find common letter
+        for (const c of p1) {
+            if (p2.indexOf(c) >= 0) {
+                result += getScore(c);
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+function part2(data) {
+    let result = 0;
+    for (let g = 0; g < data.length; g += 3) {
+        const lines = data.slice(g, g + 3);
+        // find common letter
+        for (const c of lines[0]) {
+            if (lines[1].indexOf(c) >= 0 && lines[2].indexOf(c) >= 0) {
+                result += getScore(c);
+                break;
+            }
+        }
+    }
+    return result
+}
+
+function getScore(c) {
+    if (c === c.toLowerCase()) {
+        return c.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    } else {
+        return c.charCodeAt(0) - 'A'.charCodeAt(0) + 27;
+    }
+}
+
+const data = prepareData(data1);
+console.log("Part 1: ", part1(data));
+console.log("Part 2: ", part2(data));
